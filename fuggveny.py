04a87b239385a39a6b3ff4710f864c7ace12089b
Varga-Mark-Ors <90794458+Varga-Mark-Ors@users.noreply.github.com>
2022-09-23 11:39:56 +0200

    Add files via upload

---
+++ fuggveny.py
@@ -0,0 +1,14 @@
+#def nev(v1,v2,v3,v4='hehe',v5="haha")
+def main():
+    a=64
+    b=12
+
+    while a!=b:
+        if (a>b) :
+            a=a-b
+        else:
+            b=b-a
+    
+    print(a)
+if __name__=="__main__":
+    main()
