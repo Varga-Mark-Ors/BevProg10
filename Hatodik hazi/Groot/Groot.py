0f7af772fafae11bc0e201a950c10cb4755c0f55
Varga-Mark-Ors <90794458+Varga-Mark-Ors@users.noreply.github.com>
2022-12-08 18:21:42 +0100

    Add files via upload

--- Hatodik hazi/Groot/Groot.py
+++ Hatodik hazi/Groot/Groot.py
@@ -3,7 +3,6 @@ import sys
 def main():
     a=sys.argv[0]
     b=a.replace(".py","")
-    c=b.replace("c:/Users/vmark/OneDrive/Desktop/Bevprog/BevProg10/","")
-    print(c)
+    print(b)
 if __name__ == "__main__":
     main()
